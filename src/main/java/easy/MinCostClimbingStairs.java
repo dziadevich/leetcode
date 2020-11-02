302c1f1f684dfe29b50ada0ea6a025aebe77d9b2
Dmitry Dziadevich <dziadevich@gmail.com>
2020-11-02 12:53:29 -0500

    commit

---
+++ src/main/java/easy/MinCostClimbingStairs.java
@@ -0,0 +1,24 @@
+package easy;
+
+/**
+ * On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
+ * <p>
+ * Once you pay the cost, you can either climb one or two steps.
+ * You need to find minimum cost to reach the top of the floor,
+ * and you can either start from the step with index 0, or the step with index 1.
+ */
+public class MinCostClimbingStairs {
+    public int minCostClimbingStairs(int[] cost) {
+        int f1 = 0, f2 = 0;
+        for (int i = cost.length - 1; i >= 0; --i) {
+            int f0 = cost[i] + Math.min(f1, f2);
+            f2 = f1;
+            f1 = f0;
+        }
+        return Math.min(f1, f2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new MinCostClimbingStairs().minCostClimbingStairs(new int[]{10, 15, 20}));
+    }
+}
