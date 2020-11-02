97578faede7a9df2c62f7659987a31b7c05f3f16
Dmitry Dziadevich <dziadevich@gmail.com>
2020-11-02 13:02:28 -0500

    commit

--- src/main/java/easy/MinCostClimbingStairs.java
+++ src/main/java/easy/MinCostClimbingStairs.java
@@ -8,10 +8,20 @@ package easy;
  * and you can either start from the step with index 0, or the step with index 1.
  */
 public class MinCostClimbingStairs {
+    /**
+     * Algorithm
+     *
+     * Let's evaluate f backwards in order. That way, when we are deciding what f[i] will be,
+     * we've already figured out f[i+1] and f[i+2].
+     *
+     * We can do even better than that. At the i-th step, let f1, f2 be the old value of f[i+1], f[i+2],
+     * and update them to be the new values f[i], f[i+1]. We keep these updated as we iterate through i backwards.
+     * At the end, we want min(f1, f2).
+     */
     public int minCostClimbingStairs(int[] cost) {
         int f1 = 0, f2 = 0;
-        for (int i = cost.length - 1; i >= 0; --i) {
-            int f0 = cost[i] + Math.min(f1, f2);
+        for (int value : cost) {
+            int f0 = value + Math.min(f1, f2);
             f2 = f1;
             f1 = f0;
         }
@@ -19,6 +29,6 @@ public class MinCostClimbingStairs {
     }
 
     public static void main(String[] args) {
-        System.out.println(new MinCostClimbingStairs().minCostClimbingStairs(new int[]{10, 15, 20}));
+        System.out.println(new MinCostClimbingStairs().minCostClimbingStairs(new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}));
     }
 }
