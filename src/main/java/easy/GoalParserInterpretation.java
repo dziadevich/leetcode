3260761944d0952a3a95ce2d5e39d60ec6f37a8e
Dmitry Dziadevich <dziadevich@gmail.com>
2020-12-12 11:26:56 -0500

    commit

--- src/main/java/easy/GoalParserInterpretation.java
+++ src/main/java/easy/GoalParserInterpretation.java
@@ -2,10 +2,23 @@ package easy;
 
 public class GoalParserInterpretation {
     public static String interpret(String command) {
-        return null;
+        var cArray = command.toCharArray();
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < cArray.length; i++) {
+            if (cArray[i] == 'G') {
+                str.append("G");
+            } else if (cArray[i] == '(' && cArray[i + 1] == ')') {
+                str.append("o");
+                i++;
+            } else {
+                str.append("al");
+                i += 3;
+            }
+        }
+        return str.toString();
     }
 
     public static void main(String[] args) {
-        System.out.println(GoalParserInterpretation.interpret(null));
+        System.out.println(GoalParserInterpretation.interpret("G()(al)"));
     }
 }
