b98c9cffe73552f23b171c632b766491e35279cd
Dmitry Dziadevich <dziadevich@gmail.com>
2021-03-05 20:34:28 -0500

    commit

---
+++ src/main/java/facebook/RotationalCipher.java
@@ -0,0 +1,33 @@
+package facebook;
+
+public class RotationalCipher {
+
+    public static void main(String[] args) {
+        System.out.println(new RotationalCipher().rotationalCipher("Zebra-493?", 3));
+    }
+
+    String rotationalCipher(String input, int rotationFactor) {
+        // Write your code here
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        for (char c : input.toCharArray()) {
+            if (Character.isLetter(c)) {
+                if(Character.isUpperCase(c)){
+                    i = c -'A';
+                    i = (i + rotationFactor)%26;
+                    c = (char) ('A' + i);
+                }else{
+                    i = c -'a';
+                    i = (i + rotationFactor)%26;
+                    c = (char) ('a' + i);
+                }
+            } else if (Character.isDigit(c)) {
+                i = c -'0';
+                i = (i + rotationFactor) % 10;
+                c = (char) ('0' + i);
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
