1e77adfbd5c8aab7c6c17c771d6f160e787b7b9d
Dmitry Dziadevich <dziadevich@gmail.com>
2021-03-15 23:00:44 -0400

    commit

---
+++ src/main/java/easy/LongestNiceSubstring.java
@@ -0,0 +1,26 @@
+package easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestNiceSubstring {
+
+    public static void main(String[] args) {
+        System.out.println(new LongestNiceSubstring().longestNiceSubstring("YazaAay"));
+    }
+
+    public String longestNiceSubstring(String s) {
+        if (s.length() < 2) return "";
+        char[] arr = s.toCharArray();
+        Set<Character> set = new HashSet<>();
+        for (char c : arr) set.add(c);
+        for (int i = 0; i < arr.length; i++) {
+            char c = arr[i];
+            if (set.contains(Character.toUpperCase(c)) && set.contains(Character.toLowerCase(c))) continue;
+            String sub1 = longestNiceSubstring(s.substring(0, i));
+            String sub2 = longestNiceSubstring(s.substring(i + 1));
+            return sub1.length() >= sub2.length() ? sub1 : sub2;
+        }
+        return s;
+    }
+}
