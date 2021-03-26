d6832fecf987aef024af5e1ed670a5bccaa9a09c
Dmitry Dziadevich <dziadevich@gmail.com>
2021-03-25 21:38:34 -0400

    commit

--- src/main/java/medium/TicTacToe.java
+++ src/main/java/medium/TicTacToe.java
@@ -1,4 +1,39 @@
 package medium;
 
+/*
+https://leetcode.com/problemset/all/?search=348
+ */
 public class TicTacToe {
+    public static void main(String[] args) {
+
+
+    }
+
+    public TicTacToe(int n) {
+        rows = new int[n];
+        columns = new int[n];
+    }
+
+    private int[] rows;
+    private int[] columns;
+    private int diagonalSum;
+    private int antiDiagonalSum;
+
+    public int move(int row, int col, int player) {
+        int playerValue = player == 1 ? 1 : -1;
+        if (row == col) {
+            diagonalSum += playerValue;
+        }
+        if (col == columns.length - row) {
+            antiDiagonalSum += playerValue;
+        }
+        rows[row] = playerValue;
+        columns[col] = playerValue;
+
+        if (Math.abs(diagonalSum) == 3 || Math.abs(antiDiagonalSum) == 3
+                || Math.abs(rows[row]) == 3 ||Math.abs(columns[col]) == 3)
+            return playerValue;
+
+            return 0;
+    }
 }
